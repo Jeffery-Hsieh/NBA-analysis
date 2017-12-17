1afe48e06255ab1b3e847ccd3af89591124aa197
Jeffery-Sheih <jeffery85616@gmail.com>
2017-12-17 23:47:01 +0800

    Data Prepared

--- data_preprocess.R
+++ data_preprocess.R
@@ -10,3 +10,25 @@ box_score_table_merge = function(each_game_box_score){
                                team2_box_scores = team2_players_box_scores)
   
 }
+
+game_info_mapping = function(each_month_game,game_date_table){
+  result = list()
+  for (i in seq_len(nrow(game_date_table))) {
+    PTS_1 = as.numeric(game_date_table[i,4])
+    PTS_2 = as.numeric(game_date_table[i,6])
+    Home_team_name = game_date_table[i,"Home/Neutral"]
+    Visitor_team_name = game_date_table[i,"Visitor/Neutral"]
+    winner = ifelse(PTS_1 < PTS_2,Home_team_name,Visitor_team_name)
+    
+    result[i] = list(list(game_date_table[i,"Date"],
+                          Visitor_team_name,Home_team_name,
+                          ifelse(PTS_1 < PTS_2,Home_team_name,Visitor_team_name),
+                          each_month_game[[i]][[1]],
+                          each_month_game[[i]][[2]]
+                     )
+    )
+                     
+    names(result[[i]]) = c("Date","Visitor_team","Home_team","Winner_team",paste0(Home_team_name,"_box_scores"),paste0(Visitor_team_name,"_box_scores"))
+  }
+  return(result)
+}
--- scratch_data.R
+++ scratch_data.R
@@ -11,7 +11,7 @@ source("data_preprocess.R")
 get_web_tables = function(url) {
   tables = getURL(url) %>%
   htmlParse() %>%
-  readHTMLTable(header = TRUE)
+  readHTMLTable(header = TRUE,stringsAsFactors = FALSE)
 }
 
 #get the content of desired html tag
@@ -43,6 +43,7 @@ each_month_url = paste0(NBA_url,each_month_tag_href)
 #get the table of game result
 game_result_table_list = get_web_tables(each_month_url)
 names(game_result_table_list) = get_web_content(each_month_url,xpath = '//div[@class = "filter"]/div/a')
+game_result_table_list$April = game_result_table_list$April[-96,]
 
 #each game box score url
 each_game_box_score_tag_href = each_month_url %>%
@@ -55,30 +56,38 @@ each_game_box_score_url = lapply(each_game_box_score_tag_href,function(x) paste0
 #get the analysis of game on October
 October = each_game_box_score_url[[1]]
 October_box_score_tables = lapply(as.list(October),function(x) get_web_tables(x) %>% box_score_table_merge()) 
+October_game_analysis = game_info_mapping(October_box_score_tables,game_result_table_list$October)
 
 November = each_game_box_score_url[[2]]
 November_box_score_tables = lapply(as.list(November),function(x) get_web_tables(x) %>% box_score_table_merge())
+November_game_analysis = game_info_mapping(November_box_score_tables,game_result_table_list$November)
 
 December = each_game_box_score_url[[3]]
 December_box_score_tables = lapply(as.list(December),function(x) get_web_tables(x) %>% box_score_table_merge())
+December_game_analysis = game_info_mapping(December_box_score_tables,game_result_table_list$December)
 
 January = each_game_box_score_url[[4]]
 January_box_score_tables = lapply(as.list(January),function(x) get_web_tables(x) %>% box_score_table_merge())
+January_game_analysis = game_info_mapping(January_box_score_tables,game_result_table_list$January)
 
 February = each_game_box_score_url[[5]]
 February_box_score_tables = lapply(as.list(February),function(x) get_web_tables(x) %>% box_score_table_merge())
+February_game_analysis = game_info_mapping(February_box_score_tables,game_result_table_list$February)
 
 March = each_game_box_score_url[[6]]
 March_box_score_tables = lapply(as.list(March),function(x) get_web_tables(x) %>% box_score_table_merge())
+March_game_analysis = game_info_mapping(March_box_score_tables,game_result_table_list$March)
 
 April = each_game_box_score_url[[7]]
 April_box_score_tables = lapply(as.list(April),function(x) get_web_tables(x) %>% box_score_table_merge())
+April_game_analysis = game_info_mapping(April_box_score_tables,game_result_table_list$April)
 
 May = each_game_box_score_url[[8]]
 May_box_score_tables = lapply(as.list(May),function(x) get_web_tables(x) %>% box_score_table_merge())
+May_game_analysis = game_info_mapping(May_box_score_tables,game_result_table_list$May)
+
 
 June = each_game_box_score_url[[9]]
 June_box_score_tables = lapply(as.list(June),function(x) get_web_tables(x) %>% box_score_table_merge())
-
-a = lapply(as.list(June),get_web_tables)
+June_game_analysis = game_info_mapping(June_box_score_tables,game_result_table_list$June)
 
