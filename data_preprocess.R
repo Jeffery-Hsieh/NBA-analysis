86532afdeac9079cf4e828a6fda0b46adaedcdba
Jeffery-Sheih <jeffery85616@gmail.com>
2017-12-18 11:03:28 +0800

    data_preprocess fix bug

--- data_preprocess.R
+++ data_preprocess.R
@@ -1,6 +1,6 @@
 box_score_table_merge = function(each_game_box_score){
     #
-    box_score_table = lapply(table,filter,Starters != "Reserves")
+    box_score_table = lapply(each_game_box_score,filter,Starters != "Reserves")
     
     team1_players_box_scores = merge(box_score_table[[1]],box_score_table[[2]],by = "Starters")
     team2_players_box_scores = merge(box_score_table[[3]],box_score_table[[4]],by = "Starters")
